9b299562bf443a1b2ee9bf768fb7828bbcf8e38c
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-10-02 21:52:20 -0300

    created email helper with public configs and send method

---
+++ src/helpers/email-helper.js
@@ -0,0 +1,72 @@
+"use strict";
+const environment = require("../config/environment");
+const environment_email = require("../config/email_environment");
+const nodemailer = require("nodemailer");
+const handlebars = require("handlebars");
+const reader = require("./private/template-reader");
+const validator = require("./private/email-config-validator-helper");
+
+const transporterConfig = {
+  host: environment_email.host,
+  port: environment_email.port,
+  secure: false, // true for 465, false for other ports
+  tls: {
+    ciphers: "SSLv3",
+  },
+};
+
+const mail = {
+
+  config: {
+    template: null,
+    subject: null
+  },
+
+  contact: null,
+
+  send(data) {
+
+    // Validação das configurações definidas antes do envio do email
+    validator(this.contact, this.config);
+
+    const account = environment_email.account;
+
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+      ...transporterConfig,
+      auth: {
+        user: account.username,
+        pass: account.password,
+      },
+    });
+
+    const template_path = environment.templatesDirectory 
+        + this.config.template.folder + '/'
+        + this.config.template.template;
+
+    // send mail with defined transport object
+    reader(template_path + this.config.template, (error, html) => {
+
+      const template = handlebars.compile(html);
+      const replacements = {
+        ...data,
+        appName: environment.appName,
+      };
+      const htmlToSend = template(replacements);
+      const mailOptions = {
+        from: `${environment.appName} 🪐 <${environment_email.account.username}>`,
+        to: this.contact,
+        subject: this.config.subject,
+        html: htmlToSend
+      };
+
+      // Send mail
+      transporter.sendMail(mailOptions, function (error, response) {
+        if (error)
+          console.error(error);
+      });
+    });
+  },
+};
+
+module.exports = mail;
