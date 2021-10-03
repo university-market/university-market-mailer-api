cae2206f3547c2f89b5d51c1c92ecaa325769260
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-10-02 21:59:01 -0300

    express package added

--- src/api/services/base/base-service.js
+++ src/api/services/base/base-service.js
@@ -1,9 +1,21 @@
+const mail = require('../../../helpers/email-helper');
+const template = require('../../../common/email-template');
+
 class BaseService {
 
     async sendExampleEmail(data) {
 
+        mail.config = {
+            
+            template: template.example,
+            subject: 'Primeiro e-mail base'
+        };
+        mail.contact = data.email;
+
         // Envio de email deve ser aqui
         console.log(data);
+
+        // mail.send(data);
     }
 };
 
