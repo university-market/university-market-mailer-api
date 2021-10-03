03391e052aa751c3c76334b3bebfc6d5c14448e8
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-10-02 21:05:02 -0300

    example environment variables

---
+++ src/config/environment.email.example.js
@@ -0,0 +1,9 @@
+export default {
+
+    host: 'your_host',
+    port: 999,
+    account: {
+        username: 'your_username',
+        password: 'your_password'
+    }
+};
---
+++ src/config/environment.example.js
@@ -0,0 +1,4 @@
+export default {
+
+    appName: 'University Market'
+};
