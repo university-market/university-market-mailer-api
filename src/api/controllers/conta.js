538303bb4d878529bbd1635d9381524541a79f39
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-11-15 21:13:32 -0300

    definicao entry point api de conta

--- src/api/controllers/conta.js
+++ src/api/controllers/conta.js
@@ -1,9 +1,11 @@
 const service = require('../services/conta/recuperacao-senha-service');
 
+const entryPoint = '/conta';
+
 module.exports = (app) => {
 
-    // Rota inicial exemplo do app
-    app.post('/conta/recuperarsenha', async (req, res) => {
+    // Recuperacao de senha
+    app.post(entryPoint + '/recuperarsenha', async (req, res) => {
         
         await service.sendEmailRecuperacaoSenha(req.body);
         res.end();
