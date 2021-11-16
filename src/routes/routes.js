711a4e8524761f68feee4ee09cb8c3beb3943c84
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-11-15 21:16:23 -0300

    inclusao controller api instituicao no routes loader

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,9 @@
 const routeBase = require('../api/controllers/base');
 const routeConta = require('../api/controllers/conta');
+const routeInstituicao = require('../api/controllers/instituicao');
 
 module.exports = [
     routeBase,
-    routeConta
+    routeConta,
+    routeInstituicao
 ];
